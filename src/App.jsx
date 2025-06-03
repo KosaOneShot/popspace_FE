b676841c7c637dd02218f83d5e1ebe5a747d5871
junhyeonpark <j0303p@gmail.com>
2025-06-04 00:20:11 +0900

    feat: 상태 유지용 context 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LoginForm from './pages/auth/login/LoginForm';
+import RegisterForm from './pages/auth/register/RegisterFrom';
 
 
 
@@ -10,8 +12,8 @@ function App() {
     <div>
       {/* <PageHeader /> */}
       <Routes>
-        {/* <Route path='/auth/register' element={<RegisterForm />} /> */}
-        {/* <Route path='/auth/login' element={<LoginForm />} /> */}
+        <Route path='/auth/register' element={<RegisterForm />} />
+        <Route path='/auth/login' element={<LoginForm />} />
         {/* <Route path='/main' element = {<Main /> }/> */}
       </Routes>
     </div>
---
+++ src/components/context/AuthContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import axi from '../../utils/axios/Axios';
+
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState({ role: null, nickname: null });
+
+    const [loading, setLoading] = useState(true);
+    const location = useLocation();
+    const hasChecked = useRef(false);
+
+    useEffect(() => {
+        if (hasChecked.current) return;
+        hasChecked.current = true;
+        //     if (location.pathname === '/auth/login') {
+        //   setLoading(false);
+        //   return;
+        // }
+
+        const reuslt = axi.post('/auth/check')
+            .then((res) => {
+                setAuth({
+                    role: res.data.role,
+                    nickname: res.data.nickname, // ✅ 닉네임도 함께 저장
+                });
+                console.log(res.data);
+                
+            })
+            .catch(() => {
+                setAuth({ role: null, userNickname: null });
+            })
+            .finally(() => setLoading(false));
+;
+            
+    }, [location.pathname]);
+
+    return (
+        <AuthContext.Provider value={{ ...auth, setAuth, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
