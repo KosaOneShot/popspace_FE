8d48b5695f355e9833f3d9ce96aeddedcff7a8fc
hyeesw <hyeeun7904@kookmin.ac.kr>
2025-06-06 11:29:49 +0900

    feat  : 절대경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
     server: {
     host: true, // ← 여기 추가!
     port: 5173
-  }
+  },
+  resolve: { // src 폴더를 절대 경로 시작으로 설정
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
