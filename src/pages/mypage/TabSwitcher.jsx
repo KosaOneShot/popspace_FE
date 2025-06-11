e3b848d0a487da618d6fb2b2060d6f1c95467828
ljy6712 <a01045386712@gmail.com>
2025-06-11 12:23:33 +0900

    refactor: TabSwitcher module.css 적용

--- src/pages/mypage/TabSwitcher.jsx
+++ src/pages/mypage/TabSwitcher.jsx
@@ -1,15 +1,18 @@
-// components/common/TabSwitcher.jsx
 import React from "react";
-import "./TabSwitcher.css"; // 버튼 공통 스타일 정의
+import styles from "./TabSwitcher.module.css";
 
 const TabSwitcher = ({ tabs, activeTab, onChange }) => {
   return (
     <div className="tab-wrap d-flex justify-content-center mb-3">
-      <div className="tab-group d-flex bg-light border overflow-hidden w-100">
+      <div
+        className={`${styles.tabGroup} d-flex bg-light border overflow-hidden w-100`}
+      >
         {tabs.map((tab) => (
           <button
             key={tab.key}
-            className={`tab-btn w-100 ${activeTab === tab.key ? "active" : ""}`}
+            className={`${styles.tabBtn} w-100 ${
+              activeTab === tab.key ? styles.active : ""
+            }`}
             onClick={() => onChange(tab.key)}
           >
             {tab.label}
