80a26d283fde220e5d89b74e983ea27aa97dd611
hyeesw <hyeeun7904@kookmin.ac.kr>
2025-06-11 14:10:01 +0900

    refactor : reservation 관련 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'; //bootstrap
 import { Route, Routes } from 'react-router-dom';
 import { Header, Footer, Layout } from './components'
 import { PopupDetail, PopupList } from './popup';
-import {ReservationList} from './reservation';
 import './App.css';
 import LoginForm from './pages/auth/login/LoginForm';
 import RegisterForm from './pages/auth/register/RegisterFrom';
@@ -17,7 +16,6 @@ function App() {
           <Routes>
             <Route path="/popup/detail/:popupId" element={<PopupDetail />} />
             <Route path='/popup/list' element={<PopupList/>} />
-            <Route path='/reservation/list' element={<ReservationList/>} />
             <Route path='/auth/register' element={<RegisterForm />} />
             <Route path='/auth/login' element={<LoginForm />} />
           </Routes>
