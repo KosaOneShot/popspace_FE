241ca7a46f3df13a60df5edd1fde086ca4fd72d9
hyeesw <hyeeun7904@gmail.com>
2025-06-16 16:58:43 +0900

    feat : 로그아웃 성공 모달

--- src/components/logout/LogoutButton.jsx
+++ src/components/logout/LogoutButton.jsx
@@ -1,27 +1,37 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import axi from '../../utils/axios/Axios';
 import './LogoutButton.css';
-
+import LogoutModal from './LogoutModal';
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   const handleLogout = async () => {
     try {
       await axi.post('/api/auth/logout');
-      alert('로그아웃 되었습니다.');
-      window.location.reload(); // 사이드메뉴 새로고침
-      navigate('/main');
+      // 모달부터 띄우고
+      setShowModal(true);
     } catch (error) {
       console.error('로그아웃 실패:', error);
       alert('로그아웃 중 오류가 발생했습니다.');
     }
   };
 
+  const handleModalClose = () => {
+    setShowModal(false);
+    navigate('/');
+    window.location.reload(); // 모달 닫힐 때 새로고침
+  };
+
   return (
-    <button className="logout-button" onClick={handleLogout}>
-      로그아웃
-    </button>
+    <>
+      <button className="logout-button" onClick={handleLogout}>
+        로그아웃
+      </button>
+      <LogoutModal show={showModal} onClose={handleModalClose} />
+    </>
   );
 };
 
