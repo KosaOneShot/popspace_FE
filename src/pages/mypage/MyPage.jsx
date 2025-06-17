a4ca847e847b0a72cdf18a6fd499343caa85aa88
ljy6712 <a01045386712@gmail.com>
2025-06-17 12:28:34 +0900

    fix: 마이페이지 로그인 안된 사용자가 접속하면 로그인 페이지로 이동

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import ProfileInfo from "./Profile/ProfileInfo";
 import FavoritePopups from "./FavoritePopup/FavoritePopups";
 import ReviewList from "./MyReviews/MyReviews";
-import { useLocation } from "react-router-dom";
 import MyPageAccount from "./Profile/MyPageAccount";
+import useUserInfo from "../../hook/useUserInfo";
+import { useNavigate, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./MyPage.css";
 
@@ -11,6 +11,15 @@ const MyPage = () => {
   const location = useLocation();
   const tab = location.state?.tab;
   const [activeTab, setActiveTab] = useState(tab || "profile");
+
+  const navigate = useNavigate();
+
+  const { nickname, role, loading, error } = useUserInfo();
+
+  if (!loading && (error || !nickname)) {
+    navigate("/auth/login");
+    return null;
+  }
 
   const renderTab = () => {
     switch (activeTab) {
@@ -27,10 +36,6 @@ const MyPage = () => {
 
   return (
     <div className="mypage-wrapper container py-5 px-3 mt-3">
-      {/* <h2 className="mypage-title text-center text-emerald mb-3 fw-bold">
-        마이페이지
-      </h2> */}
-
       <ul className="custom-tab-nav d-flex justify-content-around mb-3 border-bottom">
         <li
           className={`tab-item ${activeTab === "profile" ? "active" : ""}`}
@@ -46,9 +51,7 @@ const MyPage = () => {
         </li>
         <li
           className={`tab-item ${activeTab === "myReviews" ? "active" : ""}`}
-          onClick={() => {
-            setActiveTab("myReviews");
-          }}
+          onClick={() => setActiveTab("myReviews")}
         >
           내 후기
         </li>
