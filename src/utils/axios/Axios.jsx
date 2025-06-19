85d828f80a387d3600454de25cd16a20a6f355ef
junhyeonpark <j0303p@gmail.com>
2025-06-19 17:15:23 +0900

    hotfix: refresh->post벼ㄴ경

--- src/utils/axios/Axios.jsx
+++ src/utils/axios/Axios.jsx
@@ -33,7 +33,7 @@ class Axios {
                         originalRequest._retry = true;
                         
                         try {
-                            await Axios.instance.get("/api/auth/refresh");
+                            await Axios.instance.post("/api/auth/refresh");
 
                             return Axios.instance.request(originalRequest);
                         } catch (refreshError) {
